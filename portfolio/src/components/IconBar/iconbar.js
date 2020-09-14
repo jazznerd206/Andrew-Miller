f4d6cc5256641b12794e91a0208ae8b76ace3054
Andrew Miller <206alm@gmail.com>
2020-09-14 08:30:01 -0700

    basic wireframe started

---
+++ portfolio/src/components/IconBar/iconbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+function iconbar() {
+    return (
+        <div className="iconbar-wrapper">
+            <div className="iconbar-list">
+                <ul>
+                    <li>Icon 1</li>
+                    <li>Icon 2</li>
+                    <li>Icon 3</li>
+                    <li>Icon 4</li>
+                </ul>
+            </div>            
+        </div>
+    )
+}
+
+export default iconbar;
--- portfolio/src/pages/Home/home.js
+++ portfolio/src/pages/Home/home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from '../../components/Header/header';
-
-
+import IconBar from '../../components/IconBar/iconbar';
 export default function home() {
     return (
         <div>
             <Header />
+            <IconBar />
         </div>
     )
 }

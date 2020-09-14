118691a97cdb01a454cc2abb3a809942d69fe067
Andrew Miller <206alm@gmail.com>
2020-09-14 08:05:10 -0700

    initial commit

---
+++ portfolio/src/components/Header/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+
+function header() {
+    return (
+        <div className="header-wrapper">
+            <div className="header-logo">
+
+            </div>
+            <div className="header-content">
+
+            </div>
+            <div className="header-menu">
+                <ul>
+                    <li>
+                        Item 1
+                    </li>
+                    <li>
+                        Item2
+                    </li>
+                    <li>
+                        Item 3
+                    </li>
+                    <li>
+                        Item 4
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default header;
---
+++ portfolio/src/pages/Home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../components/Header/header';
+
+
+export default function home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}

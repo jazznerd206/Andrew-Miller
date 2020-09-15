dcf35a3dcea95994ccbe2287d0b28f1920cf429b
Andrew Miller <206alm@gmail.com>
2020-09-15 08:57:13 -0700

    love the minimalist. keep it up

--- portfolio/src/components/IconBar/iconbar.js
+++ portfolio/src/components/IconBar/iconbar.js
@@ -6,17 +6,42 @@ function iconbar() {
         <div className="iconbar-wrapper">
             <div className="iconbar-list">
                 <ul>
-                    <li>Change to icons:</li>
-                    <li>NodeJS</li>
-                    <li>JavaScript</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>Express</li>
-                    <li>React</li>
-                    <li>Foundation</li>
-                    <li></li>
+                    <li><i class="fas fa-coffee fa-sm"></i></li>
+                    <li><i class="fas fa-coffee fa-lg"></i></li>
+                    <li><i class="fas fa-coffee fa-2x"></i></li>
+                    <li><i class="fas fa-coffee fa-3x"></i></li>
+                    <li><i class="fab fa-node-js fa-3x"></i></li>
+                    <li><i class="fab fa-js-square fa-3x"></i></li>
+                    <li><i class="fas fa-code fa-3x"></i></li>
+                    <li><i class="fas fa-file-code fa-3x"></i></li>
+                    <li><i class="fab fa-react fa-3x"></i></li>
+                    <li><i class="fas fa-database fa-3x"></i></li>
+                    <li><i class="fas fa-mobile fa-3x"></i></li>
+                    <li><i class="fas fa-coffee fa-3x"></i></li>
+                    <li><i class="fas fa-coffee fa-2x"></i></li>
+                    <li><i class="fas fa-coffee fa-lg"></i></li>
+                    <li><i class="fas fa-coffee fa-sm"></i></li>
                 </ul>
-            </div>            
+            </div>
+            <div className="iconbar-list-flipped">
+                <ul>
+                    <li><i class="fas fa-coffee fa-sm"></i></li>
+                    <li><i class="fas fa-coffee fa-lg"></i></li>
+                    <li><i class="fas fa-coffee fa-2x"></i></li>
+                    <li><i class="fas fa-coffee fa-3x"></i></li>
+                    <li><i class="fab fa-node-js fa-3x"></i></li>
+                    <li><i class="fab fa-js-square fa-3x"></i></li>
+                    <li><i class="fas fa-code fa-3x"></i></li>
+                    <li><i class="fas fa-file-code fa-3x"></i></li>
+                    <li><i class="fab fa-react fa-3x"></i></li>
+                    <li><i class="fas fa-database fa-3x"></i></li>
+                    <li><i class="fas fa-mobile fa-3x"></i></li>
+                    <li><i class="fas fa-coffee fa-3x"></i></li>
+                    <li><i class="fas fa-coffee fa-2x"></i></li>
+                    <li><i class="fas fa-coffee fa-lg"></i></li>
+                    <li><i class="fas fa-coffee fa-sm"></i></li>
+                </ul>
+            </div>
         </div>
     )
 }

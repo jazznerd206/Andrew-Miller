74f246b0554d024483e19959a04160e882c902a6
Andrew Miller <206alm@gmail.com>
2020-09-15 15:15:35 -0700

    content required. big brain time

--- portfolio/src/components/MainContent/maincontent.js
+++ portfolio/src/components/MainContent/maincontent.js
@@ -3,17 +3,38 @@ import './style.css';
 
 function maincontent() {
     return (
+    <div>
         <div className="flex">
-            <div className="flex-item-first">
-                <span>Experience</span>
-            </div>  
-            <div className="flex-item-first">
-                <span>Experience</span>
-            </div>
-            <div className="flex-item-next">
-                <span>Bio/Image</span>
+            <span><h3>Experience</h3></span>
+            <div className="flex-item-first flex-column"> 
+                <div className="flex-column-item">
+                    <img src="https://picsum.photos/200" alt="placeholder" />
+                </div>
             </div>
         </div>
+        <div className="flex">
+            <span><h3>Projects</h3></span>
+            <div className="flex-item-first flex-column">
+                <div className="flex-column-item">
+                    <img src="https://picsum.photos/200" alt="placeholder" />
+                </div>
+                <div className="flex-column-item">
+                    <img src="https://picsum.photos/200" alt="placeholder" />
+                </div>
+                <div className="flex-column-item">
+                    <img src="https://picsum.photos/200" alt="placeholder" />
+                </div>
+            </div>
+        </div>
+        <div className="flex">
+            <span><h3>Bio</h3></span>
+            <div className="flex-item-next flex-column">
+                <div className="flex-column-item">
+                    <img src="https://picsum.photos/200" alt="placeholder" />
+                </div>
+            </div>
+        </div>
+    </div>
     )
 }
 

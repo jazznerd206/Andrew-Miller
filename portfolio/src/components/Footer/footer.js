59a3e4acb9717ace7a0d68bed5dafc4e0b5490b7
Andrew Miller <206alm@gmail.com>
2020-10-24 07:09:14 -0700

    add style to main content

--- portfolio/src/components/Footer/footer.js
+++ portfolio/src/components/Footer/footer.js
@@ -4,26 +4,28 @@ import './style.css';
 function Footer() {
     return (
         <div className="footer-wrapper">
+            <div className="title"> <h1> -- Check me out online -- </h1></div>
             <div className="footer-layer-one">
                 <a target="_blank" href="https://github.com/jazznerd206" >
                     <div className="icon-wrapper">
                         <i className="fab fa-github fa-3x footer-icon"></i>
-                        {/* <span>Github</span> */}
+                        <span>Github</span>
                     </div>
                 </a>
                 <a target="_blank" href="https://www.linkedin.com/in/andrew-miller-113a1a195/" >
                     <div className="icon-wrapper">
                         <i className="fab fa-linkedin-in fa-3x footer-icon"></i>
-                        {/* <span>LinkedIn</span> */}
+                        <span>LinkedIn</span>
                     </div>
                 </a>
                 <a target="_blank" href="https://www.instagram.com/206alm/?hl=en" >
                     <div className="icon-wrapper">
                         <i className="fab fa-instagram fa-3x footer-icon"></i>
-                        {/* <span>Instagram</span> */}
+                        <span>Instagram</span>
                     </div>
                 </a>
             </div>
+            <div></div>
             
         </div>
     )
--- portfolio/src/components/MainContent/maincontent.js
+++ portfolio/src/components/MainContent/maincontent.js
@@ -7,26 +7,26 @@ import Projects from '../Projects/Projects.js';
 function Maincontent() {
     return (
     <div className="main-content-wrapper" id="experience">
-        <div className="first-column">
+        <div className="flex-column">
             <div className="flex-item">
-                <div className="exp-title">
+                <div className="exp-title content">
                     <span><h1>Full Stack Web Developer</h1></span>
                 </div>
             </div>
-            <div className="flex-item">
+            <div className="flex-item content">
                 <div className="about-me">
                     <span>With decades of art industry experience under his belt, Andrew draws inspiration from all perspectives. A simple birdsong turned into a symphony, a grain of wood translated into glass. Applying these philosophies and working methods to development, Andrew has found a voice creating unique, beautiful and immersive experiences for all kinds of users. Combining artistic experience with technical knowledge and an insatiable desire to keep learning, he stays busy exploring new ways to translate his vision into <span className="italic"> useful, meaningful technology </span> to expose to the world.</span>
                 </div>
             </div>
         </div>
-        <div className="flex-column">
+        <div className="flex-column second-column">
             <div className="flex-item">
                 <div className="exp-title text-item-left">
                     <span><h1>Experience</h1></span>
                 </div>
             </div>
             <div className="flex-item">
-                <div className="text-item-left">
+                <div className="text-item-left content">
                     <div className="experience-title"><h2>Art:</h2></div>
                     <ul>
                         <li>20+ years experience studying, performing and recording music.</li>
@@ -35,7 +35,29 @@ function Maincontent() {
                         <li>Highly detail oriented and uncompromising standards of excellence.</li>
                     </ul>
                 </div>
-                <div className="text-item-left">
+                <div className="text-item-left content">
+                    <div className="experience-title"><h2>Certifications:</h2></div>
+                    <ul>
+                        <li>LinkedIn:
+                            <ul>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                            </ul>
+                        </li>
+                        <li>PluralSight:
+                            <ul>
+                                <li>JavaScript</li>
+                                <li>NodeJS</li>
+                            </ul>
+                        </li>
+                        <li>Coursera:
+                            <ul>
+                                <li>Data Science and Alogrithms, Duke University</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div className="text-item-left content">
                     <div className="experience-title"><h2>Technology I love:</h2></div>
                     <ul>
                         <li>JavaScript</li>
@@ -48,7 +70,7 @@ function Maincontent() {
                         <li>WebAudio API</li>
                     </ul>
                 </div>
-                <div className="text-item-left">
+                <div className="text-item-left content">
                     <div className="experience-title"><h2>Other:</h2></div>
                     <ul>
                         <li>Audio/Video editing</li>
@@ -60,11 +82,11 @@ function Maincontent() {
         </div>
         <div className="flex-column">
             <div className="flex-item">
-                <div className="exp-title text-item-right">
+                <div className="exp-title text-item-left content">
                     <span><h1>Education</h1></span>
                 </div>
             </div>
-            <div className="flex-item text-item-right">
+            <div className="flex-item text-item-left content">
                 <ul>
                     <li>Full Stack Web Development Boot Camp, University of Washington (2019-2020)</li>
                     <li>Trumpet Performance, Jazz Studies, University of Washington (2012)</li>

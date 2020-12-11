edf8c41600e7ef6677e2772c38efd60ef071c104
Andrew Miller <206alm@gmail.com>
2020-12-11 12:46:35 -0800

    more front end development

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -1,45 +1,55 @@
 import React from 'react';
 import './styles.css';
 import Resume from '../../Documents/Resume.pdf';
-import { Zoom } from 'react-reveal';
+import { Zoom, Fade, Bounce } from 'react-reveal';
 
 function About() {
     return (
         <div className="bio-container">
             <div className="flex-row">
-                <div className="bio-title">
-                    about me
-                </div>
+                
+                <Bounce bottom cascade>
+                    <div className="bio-title">
+                        about me
+                    </div>
+                </Bounce>
+                
             </div>
             <div className="flex-row">
                 {/* <div className="bio-image">
 
                 </div> */}
-                <Zoom right>
                 <div className="bio-content">
-                    <p>I believe in delivering meaningful, impactful technology paired with simple, clean code.</p>
-                    <p>I have always searched for ways to enhance existing processes or otherwise streamline production and impact quality. This built a passion for technology, first as a user, now as a developer.</p>
-                    <p className="dark-text">I seek opportunities and challenges to improve myself and assist and ally with others to approach real world problems with innovative solutions. </p>
+                    <Fade bottom>
+                        <p>I believe in delivering meaningful, impactful technology paired with simple, clean code.</p>
+                    </Fade>
+                    <Fade bottom>
+                        <p>I have always searched for ways to enhance existing processes or otherwise streamline production and impact quality. This built a passion for technology, first as a user, now as a developer.</p>
+                    </Fade>
+                    <Fade bottom>
+                        <p className="dark-text">I seek opportunities and challenges to improve myself and assist and ally with others to approach real world problems with innovative solutions. </p>
+                    </Fade>
                 </div>
-                </Zoom>
             </div>
-            <div className="flex-row header-content">
-                <a
-                    href={Resume} type="application/pdf" rel="noopener noreferrer" target="_blank"
-                >
-                    Resume
-                </a>
-                <a
-                    href="#projects"
-                >
-                    Projects
-                </a>
-                <a
-                    href="/contact"
-                >
-                    Contact me!
-                </a>
-            </div>
+            <Bounce left>
+                <div className="flex-row about-content">
+                    <a
+                        href={Resume} type="application/pdf" rel="noopener noreferrer" target="_blank"
+                    >
+                        Resume
+                    </a>
+                    <a
+                        href="#projects"
+                    >
+                        Projects
+                    </a>
+                    <a
+                        href="/contact"
+                    >
+                        Contact me!
+                    </a>
+                </div>
+            </Bounce>
         </div>
     )
 }
